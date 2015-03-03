4dd0bf3b1a95e4020dddb71170c5a3b0b3e18f9c
bouzuya <m@bouzuya.net>
2015-03-03 22:31:29 +0900

    add gulp as mocha --watch alternative

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+gulp.task('test', function() {
+  var onError = function(e) {
+      gutil.log(e);
+      this.emit('end');
+  };
+  var espower = require('gulp-espower');
+  var mocha = require('gulp-mocha');
+  return gulp.src('test/**/*.js')
+  .pipe(espower().on('error', onError))
+  .pipe(mocha().on('error', onError));
+});
+
+gulp.task('watch', function() {
+  return gulp.watch([
+    './lib/**/*.js',
+    './test/**/*.js'
+  ], ['test']);
+});
